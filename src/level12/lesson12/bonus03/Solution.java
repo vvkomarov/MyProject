016675bcf3718732eeb309a2d1e1431d6963ee75
vvkomarov <36439599+vvkomarov@users.noreply.github.com>
2018-07-17 12:07:16 +0300

    add

---
+++ src/level12/lesson12/bonus03/Solution.java
@@ -0,0 +1,43 @@
+package level12.lesson12.bonus03;
+
+/* Задача по алгоритмам
+Написать метод, который возвращает минимальное число в массиве и его позицию (индекс).
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Index of minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] data) {
+        Integer min = data[0];
+        Integer index = 0;
+        Pair<Integer, Integer> result = new Pair<>(min, index);
+
+        for (int j = 1; j < data.length; j++) {
+            if (result.x > data[j]) {
+                result.x = data[j];
+                result.y = j;
+            }
+        }
+
+        return result;
+    }
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
