d32112df84c448e5aa9b64e55a692d1344e5f01f
vvkomarov <36439599+vvkomarov@users.noreply.github.com>
2018-07-19 12:26:53 +0300

    add

---
+++ src/level13/lesson02/task10/Solution.java
@@ -0,0 +1,54 @@
+package level13.lesson02.task10;
+
+/* CleverMan и SmartGirl
+1. Добавь интерфейсы Secretary и Boss к классам CleverMan и SmartGirl. По одному на каждый. Подумай, кому какой.
+2. Унаследуй  интерфейсы Secretary и Boss от интерфейсов Person и Workable так,
+чтобы все методы у классов CleverMan и SmartGirl оказались объявленными в каком-то интерфейсе.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    interface Person {
+        void use(Person person);
+
+        void startToWork();
+    }
+
+    interface Workable {
+        boolean wantGetExtraWork();
+    }
+
+    interface Secretary extends Person {
+
+    }
+
+    interface Boss extends Person, Workable {
+
+    }
+
+    public static class CleverMan implements Boss {
+        public void use(Person person) {
+            person.startToWork();
+        }
+
+        public void startToWork() {
+        }
+
+        public boolean wantGetExtraWork() {
+            return true;
+        }
+    }
+
+    public static class SmartGirl implements Secretary {
+        public void use(Person person) {
+        }
+
+        public void startToWork() {
+        }
+    }
+
+}
