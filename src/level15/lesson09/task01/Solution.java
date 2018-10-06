3d3c2bb9d8dfeac9705511054900d8228523c510
vvkomarov <36439599+vvkomarov@users.noreply.github.com>
2018-10-06 12:37:50 +0300

    add

---
+++ src/level15/lesson09/task01/Solution.java
@@ -0,0 +1,26 @@
+package level15.lesson09.task01;
+
+/* Статики 1
+В статическом блоке инициализировать labels 5 различными парами.
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static Map<Double, String> labels = new HashMap<Double, String>();
+
+    static {
+        labels.put(1.0, "1");
+        labels.put(2.0, "2");
+        labels.put(3.0, "3");
+        labels.put(4.0, "4");
+        labels.put(5.0, "5");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(labels);
+    }
+
+}
