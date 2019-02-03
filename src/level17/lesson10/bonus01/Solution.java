bc24cd81da0b132ce448bad6cba64c27a9ea43e9
vvkomarov <36439599+vvkomarov@users.noreply.github.com>
2019-02-03 11:57:07 +0300

    add

--- src/level17/lesson10/bonus01/Solution.java
+++ src/level17/lesson10/bonus01/Solution.java
@@ -23,9 +23,12 @@ package level17.lesson10.bonus01;
         Пример параметров: -c Миронов м 15/04/1990
         */
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.Arguments;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Solution {
     public static List<Person> allPeople = new ArrayList<Person>();
@@ -37,5 +40,65 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //start here - начни тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a = reader.readLine();
+        System.out.println(args[0]);
+        StringTokenizer st = new StringTokenizer(a, " ");
+        String param = st.nextToken();
+        int id = 0;
+        String name = null;
+        String sex = null;
+        Date bd = null;
+        SimpleDateFormat format = new SimpleDateFormat("DD/mm/yyyy", Locale.ENGLISH);
+        if (param.equals("-c")) {
+            if (st.hasMoreTokens()) {
+                name = st.nextToken();
+                sex = st.nextToken();
+                bd = format.parse(st.nextToken());
+            }
+            if (sex.equals("м")) {
+                allPeople.add(Person.createMale(name, bd));
+            } else if (sex.equals("ж")) {
+                allPeople.add(Person.createFemale(name, bd));
+            }
+            System.out.println(allPeople.size());
+        }
+
+        if (param.equals("-u")) {
+            if (st.hasMoreTokens()) {
+                id = Integer.parseInt(st.nextToken());
+                name = st.nextToken();
+                sex = st.nextToken();
+                bd = format.parse(st.nextToken());
+            }
+            System.out.println("Данные до обновления : " + allPeople.get(id).getName() + " " + allPeople.get(id).getSex() + " " + allPeople.get(id).getBirthDay());
+            allPeople.get(id).setName(name);
+            if (sex.equals("м")) {
+                allPeople.get(id).setSex(Sex.MALE);
+            } else if (sex.equals("ж")) {
+                allPeople.get(id).setSex(Sex.FEMALE);
+            }
+            allPeople.get(id).setBirthDay(bd);
+            System.out.println("Данные после обновления : " + allPeople.get(id).getName() + " " + allPeople.get(id).getSex() + " " + allPeople.get(id).getBirthDay());
+        }
+
+        if (param.equals("-d")) {
+            if (st.hasMoreTokens()) {
+                id = Integer.parseInt(st.nextToken());
+            }
+            allPeople.remove(id);
+        }
+
+        if (param.equals("-i")) {
+            if (st.hasMoreTokens()) {
+                id = Integer.parseInt(st.nextToken());
+            }
+            if (id < allPeople.size()) {
+                System.out.println(allPeople.get(id).getName() + " " + allPeople.get(id).getSex() + " " + format.format(allPeople.get(id).getBirthDay()));
+            } else {
+                System.out.println("Человек с id " + id + " не найден");
+            }
+        }
+        reader.close();
     }
 }
