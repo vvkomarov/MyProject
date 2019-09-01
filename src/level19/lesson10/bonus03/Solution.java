2cb4e7fd1a91181a8501d25d92cf51a8fc001ce3
vvkomarov <36439599+vvkomarov@users.noreply.github.com>
2019-09-01 13:14:08 +0300

    add

---
+++ src/level19/lesson10/bonus03/Solution.java
@@ -0,0 +1,51 @@
+package level19.lesson10.bonus03;
+
+/* Знакомство с тегами
+Считайте с консоли имя файла, который имеет HTML-формат
+Пример:
+Info about Leela <span xml:lang="en" lang="en"><b><span>Turanga Leela
+</span></b></span><span>Super</span><span>girl</span>
+Первым параметром в метод main приходит тег. Например, "span"
+Вывести на консоль все теги, которые соответствуют заданному тегу
+Каждый тег на новой строке, порядок должен соответствовать порядку следования в файле
+Количество пробелов, \n, \r не влияют на результат
+Файл не содержит тег CDATA, для всех открывающих тегов имеется отдельный закрывающий тег, одиночных тегов нету
+Тег может содержать вложенные теги
+Пример вывода:
+<span xml:lang="en" lang="en"><b><span>Turanga Leela</span></b></span>
+<span>Turanga Leela</span>
+<span>Super</span>
+<span>girl</span>
+Шаблон тега:
+<tag>text1</tag>
+<tag text2>text1</tag>
+<tag>
+text2>text1</tag>
+text1, text2 могут быть пустыми
+*/
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader1 = new BufferedReader(new FileReader(reader.readLine()));
+        reader.close();
+        String str;
+        String result = "";
+        while ((str = reader1.readLine()) != null) {
+            result += str;
+        }
+        Document document = Jsoup.parse(result);
+        Elements elements = document.getElementsByTag(args[0]);
+        System.out.println(elements);
+    }
+}
