2f6ea46903967521012a326a1d74334047b90843
vvkomarov <Git123qweASDgit>
2019-12-08 12:42:05 +0300

    add

---
+++ src/level21/lesson05/task01/Solution.java
@@ -0,0 +1,47 @@
+package level21.lesson05.task01;
+
+/* Equals and HashCode
+В классе Solution исправить пару методов equals/hashCode в соответствии с правилами реализации этих методов.
+Метод main не участвует в тестировании.
+Требования:
+1. Хешкоды одинаковых объектов должны быть равны.
+2. Метод equals должен проверять равен ли переданный объект текущему(сравнение через ==).
+3. Метод equals должен проверять является ли переданный объект объектом класса Solution.
+4. Метод equals должен возвращать true в случае, если поля first и last равны у переданного объекта и текущего(не забудь что они могут быть равны null).
+5. Должно быть обеспечено корректное поведение HashSet с типом элементов Solution.
+*/
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+    //Метод equals должен проверять является ли переданный объект объектом класса Solution
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Solution solution = (Solution) o;
+        return Objects.equals(first, solution.first) &&
+                Objects.equals(last, solution.last);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, last);
+    }
+}
