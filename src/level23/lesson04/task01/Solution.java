0ede4d2f14fb5475db52abb138604ea03d2dbf72
vvkomarov <Git123qweASDgit>
2020-04-18 12:48:23 +0300

    add

---
+++ src/level23/lesson04/task01/Solution.java
@@ -0,0 +1,36 @@
+package level23.lesson04.task01;
+
+/*
+Inner
+Реализовать метод getTwoSolutions, который должен возвращать массив из 2-х экземпляров класса Solution.
+Для каждого экземпляра класса Solution инициализировать поле innerClasses двумя значениями.
+Инициализация всех данных должна происходить только в методе getTwoSolutions.
+Требования:
+1. В классе Solution должен быть реализован метод getTwoSolutions.
+2. Метод getTwoSolutions должен быть статическим.
+3. Метод getTwoSolutions должен быть публичным.
+4. Метод getTwoSolutions должен возвращать массив типа Solution заполненный согласно заданию.
+*/
+
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution[] res = new Solution[2];
+
+        for (int i = 0; i < 2; i++) {
+            res[i] = new Solution();
+            res[i].innerClasses[0] = res[i].new InnerClass();
+            res[i].innerClasses[1] = res[i].new InnerClass();
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
